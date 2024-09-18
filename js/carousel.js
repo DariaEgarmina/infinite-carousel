9887349e686dd9f886aac93cef928add5b420c78
Daria Egarmina <d.egarmina@gmail.com>
2024-09-18 13:55:27 +0300

    Делает движение назад с помощью анимации

--- js/carousel.js
+++ js/carousel.js
@@ -1,28 +1,49 @@
-//const nextButtonElement = document.querySelector('.slider__button--next');
+const ALL_CARDS_AMOUNT = 20;
+const SHOWN_CARDS_AMOUNT = 5;
+
 const prevButtonElement = document.querySelector('.slider__button--prev');
+//const nextButtonElement = document.querySelector('.slider__button--next');
 
 const sliderList = document.querySelector('.slider__list');
 const sliderItems = document.querySelectorAll('.slider__item');
 const sliderItem = document.querySelector('.slider__item');
 
 const itemWidth = sliderItem.offsetWidth;
+const originalTransition = getComputedStyle(sliderList).transition;
+
+for (let i = sliderItems.length - 1; i >= sliderItems.length - 5; i--) {
+  const clone = sliderItems[i].cloneNode(true);
+  sliderList.insertBefore(clone, sliderList.firstChild);
+}
+sliderList.style.transition = 'none';
+sliderList.style.transform = `translateX(${-5 * itemWidth}px)`;
 
 let currentIndex = 0;
 
 const moveSliderPrev = () => {
-  currentIndex -= 5;
-  sliderList.style.transform = `translateX(${-currentIndex * itemWidth}px)`;
-
-  for (let i = sliderItems.length - 1; i >= sliderItems.length - 5; i--) {
-    const clone = sliderItems[i].cloneNode(true);
-    sliderItems[i].remove();
-    sliderList.insertBefore(clone, sliderList.firstChild);
-  }
-
-  setTimeout(() => {
+  if (currentIndex === 0) {
+    sliderList.style.transition = originalTransition;
     sliderList.style.transform = '';
-  }, 500);
+
+    setTimeout(() => {
+      sliderList.style.transform = `translateX(${-ALL_CARDS_AMOUNT * itemWidth}px)`;
+    }, 100);
+
+    currentIndex = + 1;
+  } else if (currentIndex === 1) {
+    sliderList.style.transform = `translateX(${-(ALL_CARDS_AMOUNT - SHOWN_CARDS_AMOUNT) * itemWidth}px)`;
+
+    currentIndex = + 2;
+  } else if (currentIndex === 2) {
+    sliderList.style.transform = `translateX(${-(ALL_CARDS_AMOUNT - SHOWN_CARDS_AMOUNT * 2) * itemWidth}px)`;
+
+    currentIndex = + 3;
+  } else if (currentIndex === 3) {
+    sliderList.style.transform = `translateX(${-(ALL_CARDS_AMOUNT - SHOWN_CARDS_AMOUNT * 3) * itemWidth}px)`;
+
+    currentIndex = 0;
+  }
 };
 
+prevButtonElement.addEventListener('click', () => moveSliderPrev());
 // nextButtonElement.addEventListener('click', () => moveSlider('next'));
-prevButtonElement.addEventListener('click', () => moveSliderPrev('prev'));
