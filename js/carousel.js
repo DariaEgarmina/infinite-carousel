2589e5f173e897757f6f5d82408b64e57fce12a9
Daria Egarmina <d.egarmina@gmail.com>
2024-09-24 12:19:16 +0300

    делает кейс, когда общее количество карточек не кратно количеству отображаемых за раз

--- js/carousel.js
+++ js/carousel.js
@@ -12,20 +12,20 @@ const sliderItem = document.querySelector('.slider__item');
 const itemWidth = sliderItem.offsetWidth;
 const originalTransition = getComputedStyle(sliderList).transition;
 
-const fragmentLastCards = document.createDocumentFragment();
-const fragmentFirstCards = document.createDocumentFragment();
-
 const createClones = () => {
-  for (let i = sliderItems.length - 1, j = 0; i >= sliderItems.length - 5, j < 5; i--, j++) {
-    const cloneLastCards = sliderItems[i].cloneNode(true);
-    const cloneFirstCards = sliderItems[j].cloneNode(true);
+  const fragmentLastCards = document.createDocumentFragment();
+  const fragmentFirstCards = document.createDocumentFragment();
+
+  for (let i = 0; i < CARDS_PER_PAGE; i++) {
+    const cloneLastCards = sliderItems[sliderItems.length - CARDS_PER_PAGE + i].cloneNode(true);
+    const cloneFirstCards = sliderItems[i].cloneNode(true);
 
     fragmentLastCards.append(cloneLastCards);
     fragmentFirstCards.append(cloneFirstCards);
-
-    sliderList.prepend(fragmentLastCards);
-    sliderList.append(fragmentFirstCards);
   }
+
+  sliderList.prepend(fragmentLastCards);
+  sliderList.append(fragmentFirstCards);
 };
 
 sliderList.style.transition = 'none';
@@ -37,12 +37,33 @@ const moveSlider = (direction) => {
   sliderList.style.transition = originalTransition;
 
   if (direction === 'next') {
-    currentIndex += 5;
+    currentIndex += CARDS_PER_PAGE;
   } else {
-    currentIndex -= 5;
+    currentIndex -= CARDS_PER_PAGE;
   }
 
+  const remainder = sliderItems.length % CARDS_PER_PAGE;
+
   sliderList.style.transform = `translateX(${-(CARDS_PER_PAGE + currentIndex) * itemWidth}px)`;
+
+  if (remainder > 0) {
+    if (currentIndex === sliderItems.length - remainder) {
+      sliderList.style.transform = `translateX(${-(currentIndex + remainder) * itemWidth}px)`;
+    } else if (currentIndex === sliderItems.length + CARDS_PER_PAGE - remainder) {
+      sliderList.style.transform = `translateX(${-(currentIndex + remainder) * itemWidth}px)`;
+
+      setTimeout(() => {
+        sliderList.style.transition = 'none';
+        sliderList.style.transform = `translateX(${-CARDS_PER_PAGE * itemWidth}px)`;
+      }, 600);
+      currentIndex = 0;
+    }
+
+    if (currentIndex === -(CARDS_PER_PAGE - remainder)) {
+      sliderList.style.transform = `translateX(${-CARDS_PER_PAGE * itemWidth}px)`;
+      currentIndex = 0;
+    }
+  }
 
   if (currentIndex === sliderItems.length) {
     setTimeout(() => {
@@ -57,7 +78,7 @@ const moveSlider = (direction) => {
       sliderList.style.transition = 'none';
       sliderList.style.transform = `translateX(${-sliderItems.length * itemWidth}px)`;
     }, 600);
-    currentIndex = 15;
+    currentIndex = sliderItems.length - CARDS_PER_PAGE;
   }
 };
 
